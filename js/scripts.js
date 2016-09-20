a0eee9306aa7e3c46d1521f07ef706d5b6ad0be2
Courtney Jooste <courtney@codelab.io>
2016-09-20 13:51:30 +0200

    Finishing touches to contact page

--- js/scripts.js
+++ js/scripts.js
@@ -20,34 +20,7 @@
 
 $(document).ready(function(){
 	console.log("Ready to go!");
-	function initialize() {
-	        var myLatlng1 = new google.maps.LatLng(-34.074926, 18.816036);
-	        var mapOptions = {
-	            zoom : 15,
-	            center : myLatlng1
-	        };
-
-	        var map = new google.maps.Map(document.getElementById('map'),
-	                mapOptions);
-
-	        var contentString1 = 'Our Offices'
-
-	        var infowindow = new google.maps.InfoWindow({});
-
-	        var marker1 = new google.maps.Marker({
-	            position : myLatlng1,
-	            map : map,
-	            title : 'Park'
-	        });
-	        google.maps.event.addListener(marker1, 'click', function initialize() {
-	            infowindow.close();//hide the infowindow
-	            infowindow.setContent(contentString1);//update the content for this marker
-	            infowindow.open(map, marker1);
-
-	        });
-	    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
+	
 
 	var initialScroll = false;
 	var objectString = $('.img-brand-block')[0];
